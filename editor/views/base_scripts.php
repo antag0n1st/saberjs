a204f031e6beadde0526a1e5e35b6105a57784c1
ilija trbogazov <trbogazov@gmail.com>
2019-03-08 11:09:31 +0100

    Remove conf files

--- editor/views/base_scripts.php
+++
@@ -1,3 +0,0 @@
-<script src="config.js?<?php echo time(); ?>" type="application/javascript" ></script>
-<script src="../pixi.min.js?<?php echo time(); ?>" type="text/javascript"></script>
-<script src="../lib.min.js?<?php echo time(); ?>" type="text/javascript"></script>
